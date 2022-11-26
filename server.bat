558ef34950319b0131c1bdb17545cbcafea54758
ringerry <menshikov.artyom@bk.ru>
2022-11-27 01:03:03 +0300

    Мигающий загрузчик

--- server.bat
+++ server.bat
@@ -1,8 +1,11 @@
-@rem Непосредственно сервер
-start /b npm start
-
 @rem Предзагрузчик
 cd ./Spinner
 start /b npm start
+
+cd ..
+@rem Непосредственно сервер
+start /b npm start
+
+
 
 start http://localhost:3332
